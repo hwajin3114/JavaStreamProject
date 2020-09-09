e7a662a6112e8d9e494730613bea8111b463304d
jinimon <realpcy04@gmail.com>
2020-09-09 13:01:38 +0900

    0909

--- JavaStreamProject/src/streams/IntExample.java
+++ JavaStreamProject/src/streams/IntExample.java
@@ -11,7 +11,16 @@ public class IntExample {
 		int[] intAry = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		IntStream iStream = Arrays.stream(intAry);
 
+		System.out.println("-- Range --");
+		// 1~9. 10 포함 x
+		IntStream.range(1, 10).forEach(s -> System.out.print(s + " "));
+		System.out.println("\n-- Range Closed --");
+		// 1~10. 10 포함
+		IntStream.rangeClosed(1, 10).forEach(System.out::print);
+		System.out.println("\n-- 5보다 작은 수 --");
+		IntStream.rangeClosed(1, 10).filter(n -> n < 5).forEach(System.out::print);
+
 		long sum = iStream.filter(value -> (value % 2 == 0)).sum();
-		System.out.println("sum : " + sum);
+		System.out.println("\n1 ~ 10 sum : " + sum);
 	}
 }
